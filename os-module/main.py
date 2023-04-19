d1c85f203e2fd8e862edf9949728ae658afa8bad
michael <micha3l_255@yahoo.com>
2023-04-19 01:27:05 -0500

    os

--- os-module/main.py
+++ os-module/main.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 if __name__ == '__main__':
     file = open("sample.txt", "w+")
@@ -9,4 +8,19 @@ if __name__ == '__main__':
     print(os.listdir())
     print(os.listdir("C://Users"))
 
-    shutil.move("sample.txt", "move-here")
+    # move file
+    # shutil.move("sample.txt", "move-here")
+
+    print("OS WALK")
+    current_dir = os.getcwd()
+    for folders, sub_folders, files in os.walk(current_dir):
+        print(f"Currently looking at {folders}")
+
+        print("The sub folders are:")
+        for sub_folder in sub_folders:
+            print(f"\tSub Folder: {sub_folder}")
+
+        print("The files are")
+        for file in files:
+            print(f"\tFile: {file}")
+        print("\n")
